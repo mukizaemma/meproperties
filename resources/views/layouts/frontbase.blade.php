0ea50d918c1084ef5ca357fccc7a9d0bcc3e4413
Emma Mukiza <emmatech64@gmail.com>
2026-07-18 07:52:52 +0200

    Google analytics

--- resources/views/layouts/frontbase.blade.php
+++ resources/views/layouts/frontbase.blade.php
@@ -2,6 +2,17 @@
 <html class="no-js" lang="en">
 <base href="/public">
 <head>
+
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-18216229539"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-18216229539');
+</script>
+
     <meta charset="utf-8">
     <title>{{ $setting->company ?? '' }} </title>
     <meta name="description" content="">
